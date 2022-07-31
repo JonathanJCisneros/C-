47f65383f8d82b70038797983979610694f50137
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-31 15:31:53 -0700

    C# variables and types

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+string place = "Coding Dojo";
+
+string message = $"Hello {place}";
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Using Console.WriteLine, you can output any string to the terminal/console");
+
+Console.WriteLine("Hello from the other side :-) " + place);
+
+Console.WriteLine($"Hello World from {place}");
+
+Console.WriteLine(message);
+
+string name = "Todd";
+int age = 32;
+double height = 1.875;
+bool blueEyed = true;
+
+Console.WriteLine($"Hi my name is {name}, I am {age} years old. I am {height} tall. {blueEyed}");
