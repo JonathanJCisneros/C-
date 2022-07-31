e513403a01773a5abceb86f6a9a4c102a9d50635
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-31 16:13:05 -0700

    for/while loops

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -46,3 +46,19 @@ if (!crazy){
     Console.WriteLine("Let's party!");
 }
 
+for(int i = 1; i <=5; i++){
+    Console.WriteLine(i);
+}
+
+int start = 0;
+int end = 12;
+
+for(int i = start; i < end; i ++){
+    Console.WriteLine(i);
+}
+
+int j = 1;
+while(j < 6){
+    Console.WriteLine(j);
+    j++;
+}
