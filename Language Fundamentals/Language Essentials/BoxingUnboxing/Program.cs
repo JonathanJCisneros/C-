1437bf01214024659a1b9290c560ae23f7155004
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-08-01 12:09:23 -0700

    Boxing/Unboxing Assignment

---
+++ Language Fundamentals/Language Essentials/BoxingUnboxing/Program.cs
@@ -0,0 +1,15 @@
+﻿List<object> NewBox = new List<object>(){7, 28, -1, true, "chair"};
+
+foreach(object value in NewBox){
+    Console.WriteLine(value);
+}
+
+int sum = 0;
+
+foreach(object value in NewBox){
+    if(value is int){
+        sum = sum + (int)value;
+    }
+}
+
+Console.WriteLine(sum);
