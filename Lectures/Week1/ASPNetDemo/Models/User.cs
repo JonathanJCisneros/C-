dfe729d9079ea08d0509e2fd92dfb1e64746815b
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-08-04 11:02:24 -0700

    .

--- Lectures/Week1/ASPNetDemo/Models/User.cs
+++ Lectures/Week1/ASPNetDemo/Models/User.cs
@@ -1,8 +1,10 @@
+#pragma warning disable CS8618
+
 namespace ASPNetDemo.Models;
 
 public class User
 {
-    public string FirstName {get; set;}
+    public string FirstName {get; set;} 
     public string LastName {get; set;}
     public int Age {get; set;}
 
