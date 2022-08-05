9b5fd9776a20eff391cfc0e65b56b1c0db8cde3c
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-08-04 18:40:39 -0700

    Random Passcode Part I

---
+++ ASP .NET Core/ASP MVC II/RandomPasscode/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+app.Run();
