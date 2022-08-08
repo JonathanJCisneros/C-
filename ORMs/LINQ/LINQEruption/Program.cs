5c2edb91a9a4ec1c11a569ba2f184ee9e4b9c69d
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-08-08 13:29:06 -0700

    LINQ Eruption assignment

--- ORMs/LINQ/LINQEruption/Program.cs
+++ ORMs/LINQ/LINQEruption/Program.cs
@@ -17,19 +17,80 @@ List<Eruption> eruptions = new List<Eruption>()
     new Eruption("Bardarbunga", 1477, "Iceland", 2000, "Stratovolcano")
 };
 // Example Query - Prints all Stratovolcano eruptions
-IEnumerable<Eruption> stratovolcanoEruptions = eruptions.Where(c => c.Type == "Stratovolcano");
-PrintEach(stratovolcanoEruptions, "Stratovolcano eruptions.");
-// Execute Assignment Tasks here!
+// IEnumerable<Eruption> stratovolcanoEruptions = eruptions.Where(c => c.Type == "Stratovolcano");
+// PrintEach(stratovolcanoEruptions, "Stratovolcano eruptions.");
+// // Execute Assignment Tasks here!
 
-IEnumerable<Eruption> FirstChile = eruptions.First(c => c.Location == "Chile");
-PrintEach(FirstChile, "First Eruption in Chile");
 
-// Helper method to print each item in a List or IEnumerable.This should remain at the bottom of your class!
-static void PrintEach(IEnumerable<dynamic> items, string msg = "")
+// // Helper method to print each item in a List or IEnumerable.This should remain at the bottom of your class!
+// static void PrintEach(IEnumerable<dynamic> items, string msg = "")
+// {
+//     Console.WriteLine("\n" + msg);
+//     foreach (var item in items)
+//     {
+//         Console.WriteLine(item.ToString());
+//     }
+// }
+
+// First chile eruption
+Eruption FirstChile = eruptions.First(eruption => eruption.Location == "Chile");
+Console.WriteLine(FirstChile);
+
+// First Hawaiian Is eruption
+Eruption? FirstHawaiian = eruptions.First(eruptions => eruptions.Location == "Hawaiian Is");
+if(FirstHawaiian != null)
 {
-    Console.WriteLine("\n" + msg);
-    foreach (var item in items)
-    {
-        Console.WriteLine(item.ToString());
-    }
+    Console.WriteLine(FirstHawaiian);
 }
+else
+{
+    Console.WriteLine("Hawaiian Is not found");
+}
+
+// New Zealand eruption > year 1900
+IEnumerable<Eruption> NewZealand = eruptions.Where(eruption => eruption.Year > 1900);
+Console.WriteLine(NewZealand.First());
+
+// Eruptions higher than 2000m
+IEnumerable<Eruption> heightTwoThousand = eruptions.Where(eruption => eruption.ElevationInMeters > 2000);
+
+foreach (Eruption item in heightTwoThousand)
+{
+    Console.WriteLine(item);
+}
+
+// starts with Z
+IEnumerable<Eruption> startsWithZ = eruptions.Where(eruption => eruption.Volcano.StartsWith("Z"));
+foreach (Eruption item in startsWithZ)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine($"These are how many eruptions from volcanoes that start with 'Z': {startsWithZ.Count()}" );
+
+// highest elevation
+int? highest = eruptions.Max(e => e.ElevationInMeters);
+Console.WriteLine(highest);
+
+// volcano Name
+Eruption volName = eruptions.First(e => e.ElevationInMeters == highest);
+Console.WriteLine(volName.Volcano);
+
+// All Volcanoes by alphabet
+IEnumerable<Eruption> allVolcanoes = eruptions.OrderBy(e => e.Volcano);
+foreach (Eruption item in allVolcanoes)
+{
+    Console.WriteLine(item.Volcano);
+}
+
+// Print all
+IEnumerable<Eruption> before1000 = eruptions.Where(e => e.Year < 1000).OrderBy(e => e.Volcano);
+foreach (Eruption item in before1000)
+{
+    Console.WriteLine(item);
+};
+
+foreach (Eruption item in before1000)
+{
+    Console.WriteLine(item.Volcano);
+};
+
