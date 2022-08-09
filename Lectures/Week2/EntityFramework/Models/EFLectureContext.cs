a3f82b0e6e1c6f24e113663c4c91e6e637fdc681
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-08-09 10:53:00 -0700

    .

--- Lectures/Week2/EntityFramework/Models/EFLectureContext.cs
+++ Lectures/Week2/EntityFramework/Models/EFLectureContext.cs
@@ -6,6 +6,6 @@ namespace EntityFramework.Models;
 public class EFLectureContext : DbContext 
 { 
     public EFLectureContext(DbContextOptions options) : base(options) { }
-    // the "Monsters" table name will come from the DbSet property name
+
     public DbSet<Post> Posts { get; set; } 
 }
