c93a0d3185a138b742b1a4945efe08ceee82124e
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-08-10 14:08:33 -0700

    Login and Registration Assignment

--- ORMs/Entity Framework/LoginAndRegistration/Controllers/UserController.cs
+++ ORMs/Entity Framework/LoginAndRegistration/Controllers/UserController.cs
@@ -35,8 +35,92 @@ public class UserController : Controller
     {
         if(loggedIn)
         {
-            return RedirectToAction("All", "Post");
+            return Success();
         }
         return View("Register");
+    }
+
+
+    [HttpPost("/register")]
+    public IActionResult Register(User newUser)
+    {
+        if(ModelState.IsValid)
+        {
+            if(db.Users.Any(user => user.Email == newUser.Email))
+            {
+                ModelState.AddModelError("Email", "is taken");
+            }
+        }
+
+        if(ModelState.IsValid == false)
+        {
+            return Index();
+        }
+
+        PasswordHasher<User> hashBrowns = new PasswordHasher<User>();
+        newUser.Password = hashBrowns.HashPassword(newUser, newUser.Password);
+        db.Users.Add(newUser);
+        db.SaveChanges();
+        HttpContext.Session.SetInt32("userId", newUser.UserId);
+        return Success();
+    }
+
+
+    [HttpGet("/login")]
+    public IActionResult Enter()
+    {
+        if(loggedIn)
+        {
+            return Success();
+        }
+        return View("Login");
+    }
+
+
+    [HttpPost("/loggingin")]
+    public IActionResult Login(LoginUser user)
+    {
+        if(ModelState.IsValid == false)
+        {
+            return Enter();
+        }
+
+        User? dbUser = db.Users.FirstOrDefault(User => User.Email == user.LoginEmail);
+
+        if(dbUser == null)
+        {
+            ModelState.AddModelError("LoginEmail", "not found");
+            return Enter();
+        }
+
+        PasswordHasher<LoginUser> hashBrowns = new PasswordHasher<LoginUser>();
+        PasswordVerificationResult pwCheck = hashBrowns.VerifyHashedPassword(user, dbUser.Password, user.LoginPassword);
+
+        if(pwCheck == 0)
+        {
+            ModelState.AddModelError("LoginPassword", "is not correct");
+            return Enter();
+        }
+
+        HttpContext.Session.SetInt32("userId", dbUser.UserId);
+        return Success();
+    }
+
+
+    [HttpGet("/success")]
+    public IActionResult Success()
+    {
+        if(!loggedIn)
+        {
+            return Enter();
+        }
+        return View("Success");
+    }
+
+    [HttpPost("/logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return Enter();
     }
 }
