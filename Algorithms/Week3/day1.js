9809f3a16238c08b468873e156dae071f7bc8aa6
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-08-16 09:20:58 -0700

    morning algorithm

--- Algorithms/Week3/day1.js
+++ Algorithms/Week3/day1.js
@@ -13,15 +13,40 @@ class MinHeap {
     }
 
 
-    top() {
-        return this.heap.length > 1? this.heap[1]: null;
+    idxOfParent(i) {
+        return Math.floor(i / 2);
     }
 
 
-    insert(num){
-        this.heap.push(num)
-        let currentIdx = this.heap.length - 1;
-        while(this.heap[currentIdx] < this.heap[Math.floor(currentIdx/2)]){
+    idxOfLeftChild(i) {
+        return i * 2;
+    }
+
+
+    idxOfRightChild(i) {
+        return i * 2 + 1;
+    }
+
+    size(){
+        return this.heap.length;
+    }
+
+
+    swap(i, j) {
+        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+    }
+
+
+    extract() {
+        let temp = this.heap[1];
+        this.heap[1] = this.heap[this.heap.length-1];
+        this.heap[this.heap.length-1] = temp;
+        this.heap.pop();
+        let currentIdx = 1;
+        
+        console.log(currentIdx)
+        console.log(temp)
+        while(this.heap[currentIdx] > this.heap[Math.floor(currentIdx/2)]){
             let temp = this.heap[Math.floor(currentIdx/2)]
             this.heap[Math.floor(currentIdx/2)] = this.heap[currentIdx]
             this.heap[currentIdx] = temp;
@@ -29,6 +54,36 @@ class MinHeap {
         }
     }
 
+
+
+    top() {
+        return this.heap.length > 1? this.heap[1]: null;
+    }
+
+
+    insert(num){
+        this.heap.push(num);
+        this.shiftUp();
+        return this.size();
+    }
+
+    shiftUp() {
+        let idxOfNodeToShiftUp = this.heap.length - 1;
+
+        while (idxOfNodeToShiftUp > 1) {
+            const idxOfParent = this.idxOfParent(idxOfNodeToShiftUp);
+
+            const isParentSmallerOrEqual =
+            this.heap[idxOfParent] <= this.heap[idxOfNodeToShiftUp];
+
+            if (isParentSmallerOrEqual) {
+            break;
+            }
+
+            this.swap(idxOfNodeToShiftUp, idxOfParent);
+            idxOfNodeToShiftUp = idxOfParent;
+        }
+    }
 
     printHorizontalTree(parentIdx = 1, spaceCnt = 0, spaceIncr = 10) {
         if (parentIdx > this.heap.length - 1) {
@@ -52,6 +107,8 @@ const newHeap = new MinHeap();
 newHeap.insert(5)
 newHeap.insert(7)
 newHeap.insert(3)
+newHeap.insert(10)
+newHeap.insert(12)
 
 newHeap.printHorizontalTree()
 
